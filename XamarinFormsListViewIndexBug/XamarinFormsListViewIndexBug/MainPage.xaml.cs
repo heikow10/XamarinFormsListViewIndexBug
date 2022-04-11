d06879499a1d3654c037d9fb1c649bf78709d9ca
Heiko Wobst <wobst.heiko1010@t-online.de>
2022-04-11 21:02:18 +0200

    add event handlers and context action to test item indices

--- XamarinFormsListViewIndexBug/XamarinFormsListViewIndexBug/MainPage.xaml.cs
+++ XamarinFormsListViewIndexBug/XamarinFormsListViewIndexBug/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace XamarinFormsListViewIndexBug
@@ -15,5 +16,23 @@ namespace XamarinFormsListViewIndexBug
         }
 
         public ObservableCollection<string> Items { get; }
+
+        private async void ListView_ItemSelectedAsync(object sender, SelectedItemChangedEventArgs e)
+        {
+            await DisplayAlert("ListView_ItemSelected", "e.SelectedItem = " + e.SelectedItem + ", e.SelectedItemIndex = " + e.SelectedItemIndex, "OK");
+        }
+
+        private async void ListView_ItemTappedAsync(object sender, ItemTappedEventArgs e)
+        {
+            await DisplayAlert("ListView_ItemTapped", "e.Item = " + e.Item + ", e.ItemIndex = " + e.ItemIndex, "OK");
+        }
+
+        private async void MenuItem_ClickedAsync(object sender, EventArgs e)
+        {
+            if (sender is MenuItem menuItem && menuItem.BindingContext is string item)
+            {
+                await DisplayAlert("MenuItem_Clicked", "item = " + item, "OK");
+            }
+        }
     }
 }
